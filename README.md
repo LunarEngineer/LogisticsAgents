06bc15ad4be035a5616077b640c70e2b2ba7c45b
Timothy Luna <timothy.d.luna@gatech.edu>
2019-08-06 09:07:06 -0500

    Working agent with output for simple agent

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # LogisticsAgents
+
 This is a series of agents intended for use in the OpenAI Simple Logistics environment
 
 This requires the installation of the logistics environment, which may be found at https://github.com/LunarEngineer/gym-logistics_simple.git.
 
+## Simple Agent
+
+This agent is meant to serve as a counterpoint and an example to conduct analysis against. It is *not* a reinforcement learner and is just a very small and simple conditional logic implementation.
+
+In every time step the agent considers the pool of available trucks and for each truck computes the 'needs' for customers by calculating the deficit from the maximum supply for that customer and then masking by what the truck is capable of carrying. The remaining needs are sorted by magnitude and the truck dispatched to service the largest request. For a concrete example imagine that a customer named "Bob" can carry fuel and water and can carry 100 units of each but at a specific moment in time has 75 fuel and 60 water. Another customer named "Alice" can carry the same supply and amount of supply but has 25 fuel and 70 water,
+
+Bob = [75,60]
+Alice = [25,70]
+
+Bob's 'needs' are [25,40] while Alice's are [75,30].
+
+John is driving a water truck (which better not be carrying fuel!) and currently has 400 units of water. John sees Bob's needs effectively as [0,40] and Alice's as [0,30] and will dispatch to service Bob.
+
+Each truck that is not currently dispatched will be dispatched; that is the 'action' for this environment.
+
+When a truck reaches a customer that customer is considered serviced and the agent allowed to reassign the truck.
+
+The images below show (on the left) a simple flow diagram for the decision making process and (on the right) a sample episode of 1000 time steps where the agent is servicing a series of customers. The customers 'deficit' is shown by the solid sections; a smaller solid section means that the customer is better supplied. The trucks mileage is shown by the lines; this agent is continually dispatching and the trucks never stop moving and as a result their mileage monotonically increases by the speed of the truck (for the most part).
+
+<div class="row">
+<div class="col-sm-6">
+![Flow Diagram](SimpleAgentDiagram.PNG "Simple Agent Decision Diagram")
+</div>
+<div class="col-sm-6">
+![Effectiveness Diagram](SimpleAgent.PNG "Simple Agent Effectiveness")
+</div>
+</div>
--- SingleTargetAgent.py
+++ SingleTargetAgent.py
@@ -1,6 +1,16 @@
 import gym
 from math import sqrt
 import numpy as np
+import pandas as pd
+
+def prettyArr(a):
+  """
+  This silly function is just to pretty print numeric arrays
+  in case you are doing some debugging.
+  """
+  for row in a:
+    rowstr = ",".join(["{:.1f}".format(x) for x in row])
+    print("\t{}".format(rowstr))
 
 def getDesire(c):
   """
@@ -9,6 +19,7 @@ def getDesire(c):
   """
   desired_supply = np.full(c.supply_classes,c.supply_limit) - c.supplies
   return(desired_supply)
+
 def getMission(trk,customer_needs,mission: int=None):
   """
   This helper function takes in a truck and a two-dimensional numpy
@@ -20,7 +31,8 @@ def getMission(trk,customer_needs,mission: int=None):
   # This is necessary to input into the action space.
   pri = np.array(trk.supply_priority * 255, dtype='uint8')
   # The mission flag is passed to handle pre-existing missions
-  if mission:
+  if mission is not None:
+    # print("Mission has been passed as {}".format(mission))
     cust = mission
   else:
     # Do an initial check to see if this truck needs to restock.
@@ -48,74 +60,78 @@ def getMission(trk,customer_needs,mission: int=None):
         cust = np.argmax(serviceable)
   # Return the customer number and append the current supply priority.
   #  Note that this agent makes *no* attempt to modify supply priority.
+  # if mission:
+  #   print("Mission will be returned as {}".format(np.concatenate((np.atleast_1d(cust),pri))))
   return(np.concatenate((np.atleast_1d(cust),pri)))
 
-vfunc = np.vectorize(getDesire)
 # This agent keeps track of customer needs and dispatches trucks accordingly.
 env = gym.make('gym_logistics_simple:logistics-simple-v0')
-
+# Create an empty dictionary to store run information
+outInfo = {}
 # How many simulations am I going to run?
-for i in range(1):
-  env.reset()
-  done = False
-  # Initialize the dispatch status to 'no orders' for all trucks  
-  # What trucks are available? this will set a row key for the
-  # dispatch status matrix.
-  trkInd = {k:t for k,t in zip([x for x in range(len(env.trucks))],[t for t in env.trucks])}
-  # Initialize the customers
+env.reset()
+done = False
+# Initialize the dispatch status to 'no orders' for all trucks  
+# What trucks are available? this will set a row key for the
+# dispatch status matrix.
+trkInd = {k:t for k,t in zip([x for x in range(len(env.trucks))],[t for t in env.trucks])}
+# Initialize the customers
+customer_needs = np.vstack([getDesire(env.customers[c]) for c in env.customers])
+# Initialize all the truck orders.
+dispatch_status = np.full(env.action_space.shape,customer_needs.shape[0]+1,dtype='uint8')
+# Create a dummy first state
+s = env._next_observation()
+while not done:
+  # Update the customers by calculating their deficit across supply.
+  # Note that the agent will NOT have this, they will simply have remaining supply
+  # and indirectly will have that relationship to max supply via the calculated
+  # reward.
   customer_needs = np.vstack([getDesire(env.customers[c]) for c in env.customers])
-  # Initialize all the truck orders.
-  dispatch_status = np.full(env.action_space.shape,customer_needs.shape[0]+1,dtype='uint8')
-  # Create a dummy first state
-  s = env._next_observation()
-  while not done:
-    # Update the customers by calculating their deficit across supply.
-    # Note that the agent will NOT have this, they will simply have remaining supply
-    # and indirectly will have that relationship to max supply via the calculated
-    # reward.
-    customer_needs = np.vstack([getDesire(env.customers[c]) for c in env.customers])
-    # Consider the trucks that are currently dispatched to service
-    #  update the customer needs to reflect that they *will* be serviced
-    #  and route trucks appropriately. Do a check here to unassign a truck
-    #  if they've delivered
-    for trk_i in range(len(dispatch_status)):
-      trk = env.trucks[trkInd[trk_i]]
-      # Update customer needs
-      customer_number = dispatch_status[trk_i,0]
-
-      if customer_number < customer_needs.shape[0]:
-        # Is the truck currently at the customer?
-        # Do a quick check for mission complete
-
-        x_t,y_t = s[customer_number][0:2]
-        x_0,y_0 = trk.location
-        if sqrt((x_t-x_0)**2+(y_t-y_0)**2) < 1e-3:
-          dispatch_status[trk_i][0] = customer_needs.shape[0] + 1
-        else:
-          newarr = np.maximum(0,customer_needs[customer_number]-trk.supplies)
-          customer_needs[customer_number] = newarr
-      elif customer_number == customer_needs.shape[0]:
-          x_t,y_t = env.depot
-          x_0,y_0 = trk.location
-          if sqrt((x_t-x_0)**2+(y_t-y_0)**2) < 1e-3:
-            dispatch_status[trk_i][0] = customer_needs.shape[0] + 1
-    print("\t\tCUSTOMERNEEDS: {}".format(customer_needs))
-    for trk_i in range(len(dispatch_status)):
-      trk = env.trucks[trkInd[trk_i]]
-      # Update customer needs
-      customer_number = dispatch_status[trk_i,0]
-      if customer_number > customer_needs.shape[0]:
-        newarr = getMission(trk,customer_needs)
-        dispatch_status[trk_i] = newarr
-        customer_number = newarr[0]
-        # Does this truck have a customer now?
-        if customer_number < customer_needs.shape[0]:
-          customer_needs[customer_number] = np.maximum(0,customer_needs[customer_number]-trk.supplies)
+  # Consider the trucks that are currently dispatched to service
+  #  update the customer needs to reflect that they *will* be serviced
+  #  and route trucks appropriately. Do a check here to unassign a truck
+  #  if they've delivered
+  # Update customer needs and 'undispatch' trucks if possible
+  for trk_i in range(len(dispatch_status)):
+    trk = env.trucks[trkInd[trk_i]]
+    customer_number = dispatch_status[trk_i,0]
+    if customer_number < customer_needs.shape[0]:
+      # Is the truck currently at the customer?
+      # Do a quick check for mission complete
+      # How far from the customer is the truck?
+      x_t, y_t = s[customer_number][0:2]
+      x_0, y_0 = trk.location
+      if sqrt((x_t-x_0)**2+(y_t-y_0)**2) < 1e-3:
+        # If the truck is close to the target then give an 'illegal' target
+        dispatch_status[trk_i][0] = customer_needs.shape[0] + 1
       else:
-        dispatch_status[trk_i] = getMission(trk,customer_needs,customer_number)
-    print("\t\tDISPATCH: {}".format(dispatch_status))
-    # What actions do I need to take?
-    s,r,done,info = env.step(dispatch_status)
-
-    env.render()
+        # Otherwise update the needs for that customers line.
+        newarr = np.maximum(0,customer_needs[customer_number]-trk.supplies)
+        customer_needs[customer_number] = newarr
+    elif customer_number == customer_needs.shape[0]:
+      # This is just here because the depot isn't a customer object
+      #  and so it needs a different accounting for location.
+      x_t, y_t = env.depot
+      x_0, y_0 = trk.location
+      if sqrt((x_t-x_0)**2+(y_t-y_0)**2) < 1e-3:
+        dispatch_status[trk_i][0] = customer_needs.shape[0] + 1
+  for trk_i in range(len(dispatch_status)):
+    trk = env.trucks[trkInd[trk_i]]
+    # Update customer needs
+    customer_number = dispatch_status[trk_i,0]
+    if customer_number > customer_needs.shape[0]:
+      newarr = getMission(trk,customer_needs)
+      dispatch_status[trk_i] = newarr
+      customer_number = newarr[0]
+      # Does this truck have a customer now?
+      if customer_number < customer_needs.shape[0]:
+        customer_needs[customer_number] = np.maximum(0,customer_needs[customer_number]-trk.supplies)
+    else:
+      dispatch_status[trk_i] = getMission(trk,customer_needs,customer_number)
+  # What actions do I need to take?
+  s,r,done,info = env.step(dispatch_status)
+  outInfo[env.current_step] = env.get_information()
+  # This can be turned on to render
+  # env.render()
 env.close()
+pd.DataFrame.from_dict(outInfo,orient='index').to_csv("out.csv")
